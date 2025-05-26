eee4b7967e2bf1efd01a0599fe00058adc46d2ea
Stuart P. Bentley <s@stuartpb.com>
2025-05-26 06:31:04 +0000

    Add CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://quidspec.org',
   integrations: [
     starlight({
       title: 'QUID',
